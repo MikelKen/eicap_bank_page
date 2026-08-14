a0040052a3906d85476c60a6ce14c176cd5d99cf
Mikelken <mayki6866@gmail.com>
2026-08-14 15:30:58 -0400

    fix: delete vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
@@ -14,6 +15,7 @@ const config = defineConfig({
         enabled: true,
       },
     }),
+    nitro(),
     viteReact(),
   ],
 });
